2f187e59a4becff7bc337604c45d8de243a74eab
Waris Ali <m.warisali2@gmail.com>
2018-07-04 08:44:12 +0500

    Ignore .vscode file

--- twitter.py
+++ twitter.py
@@ -1,20 +1,27 @@
+#!/usr/bin/env python3
+
 import sys
 
-
-#Function to exit program with printing error
+# Function to exit program with printing error
 def pexit(str):
     print("Error: " + str)
     exit()
 
-#Main function
+# Main function
 def main():
-    
-    #checking passed arguments
-    if(len(sys.argv) != 3):
-        pexit("Insufficient number of arguments!")
 
-    keyword1 = sys.argv[1]
-    keyword2 = sys.argv[2]
+    keyword1 = ""
+    keyword2 = ""
+
+    # checking passed arguments
+    if(len(sys.argv) == 1):
+        keyword1 = input("Enter first keyword: ")
+        keyword2 = input("Enter second keyword: ")
+    elif(len(sys.argv) == 3):
+        keyword1 = sys.argv[1]
+        keyword2 = sys.argv[2]
+    else:
+        pexit("Insufficient number of arguments!")
 
     print("First arg: {}\nSecond arg: {}".format(keyword1, keyword2))
 
